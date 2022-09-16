be05718134109ca01dff42922a24803b7155ddc3
tinet-wanghm <wanghm@ti-net.com.cn>
2022-09-16 17:06:52 +0800

    feat: env环境变量枚举定义

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+// 用于 TypeScript 的环境变量智能提示配置
+interface ImportMetaEnv {
+  // 当前环境
+  readonly VITE_ENV: string
+  // 公共基础路径
+  readonly VITE_BASE_URL: string
+  // api base地址
+  readonly VITE_API_URL: string
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
+export default ({ mode }) => {
+  const envData = loadEnv(mode, "./vite-environment")
+  console.log("获取到环境变量：", mode, envData)
+  return defineConfig({
+    envDir: "./vite-environment",
+    base: "./",
+    define: {
+      "process.env": loadEnv(mode, process.cwd())
+    },
+    plugins: [vue()],
+    resolve: {
+      // 路径别名
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url))
+      }
     }
-  }
-})
+  })
+}
