d961475438767fc0c89ffa25684163e684301d9e
fyxs <867952383@qq.com>
2022-09-19 16:26:04 +0800

    feat: 环境变量配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,24 @@ import { fileURLToPath, URL } from "node:url"
 
 import { defineConfig, loadEnv } from "vite"
 import vue from "@vitejs/plugin-vue"
+import VueSetupExtend from "vite-plugin-vue-setup-extend" // 允许使用 setup 语法时自定义组件 name
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
+export default defineConfig(({ command, mode }) => {
   const envData = loadEnv(mode, "./vite-environment")
-  console.log("获取到环境变量：", mode, envData)
-  return defineConfig({
+  console.log("获取到环境变量：", command, mode, envData)
+  return {
     envDir: "./vite-environment",
     base: "./",
     define: {
       "process.env": loadEnv(mode, process.cwd())
     },
-    plugins: [vue()],
+    plugins: [vue(), VueSetupExtend()],
     resolve: {
       // 路径别名
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url))
       }
     }
-  })
-}
+  }
+})
